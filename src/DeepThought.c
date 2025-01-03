23e9c35ac3b02cea057c4115aeea6de8355084a7
Max Horn <max@quendi.de>
2025-01-03 03:00:19 +0100

    Use designated initializers for StructInitInfo

--- src/DeepThought.c
+++ src/DeepThought.c
@@ -240,18 +240,11 @@ static Int InitLibrary( StructInitInfo *module )
 *F  InitInfopl()  . . . . . . . . . . . . . . . . . table of init functions
 */
 static StructInitInfo module = {
- /* type        = */ MODULE_DYNAMIC,
- /* name        = */ "DeepThought",
- /* revision_c  = */ 0,
- /* revision_h  = */ 0,
- /* version     = */ 0,
- /* crc         = */ 0,
- /* initKernel  = */ InitKernel,
- /* initLibrary = */ InitLibrary,
- /* checkInit   = */ 0,
- /* preSave     = */ 0,
- /* postSave    = */ 0,
- /* postRestore = */ PostRestore
+    .type = MODULE_DYNAMIC,
+    .name = "DeepThought",
+    .initKernel = InitKernel,
+    .initLibrary = InitLibrary,
+    .postRestore = PostRestore,
 };
 
 StructInitInfo *Init__Dynamic( void )
